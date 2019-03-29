a62cad0c67a5f44c184f0bfbc33763102aafade5
ShadowMole <steven.bruman@yahoo.com>
2019-03-29 18:05:48 -0400

    Old Maze

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	private MazeCell currentCell;
+
+	public void SetLocation(MazeCell cell){
+		currentCell = cell;
+	}
+}
---
+++ Assets/Scripts/SpookyLightSystem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpookyLightSystem : MonoBehaviour {
+
+	public void SetPosition(Vector3 pos){
+		this.transform.position = pos;
+	}
+}
